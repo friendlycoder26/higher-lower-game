344035894f2e64a562c1a47a7cb31bda12104198
ABHISHEK SHARMA <abhi95018sharma@gmail.com>
2025-12-06 12:22:09 +0530

    Add files via upload

---
+++ dictonaries.py
@@ -0,0 +1,54 @@
+data = [
+    {"name": "Cristiano Ronaldo", "followers": 630, "description": "Footballer"},
+    {"name": "Lionel Messi", "followers": 520, "description": "Footballer"},
+    {"name": "Selena Gomez", "followers": 430, "description": "Singer & Actress"},
+    {"name": "Kylie Jenner", "followers": 400, "description": "Model & Businesswoman"},
+    {"name": "Dwayne Johnson", "followers": 395, "description": "Actor"},
+    {"name": "Ariana Grande", "followers": 380, "description": "Singer"},
+    {"name": "Kim Kardashian", "followers": 365, "description": "Media Personality"},
+    {"name": "Virat Kohli", "followers": 270, "description": "Cricketer"},
+    {"name": "Taylor Swift", "followers": 285, "description": "Singer"},
+    {"name": "Beyoncé", "followers": 320, "description": "Singer"},
+    {"name": "Justin Bieber", "followers": 290, "description": "Singer"},
+    {"name": "Neymar Jr.", "followers": 220, "description": "Footballer"},
+    {"name": "Zendaya", "followers": 195, "description": "Actress"},
+    {"name": "Katy Perry", "followers": 205, "description": "Singer"},
+    {"name": "Drake", "followers": 145, "description": "Rapper"},
+    {"name": "Chris Hemsworth", "followers": 110, "description": "Actor"},
+    {"name": "Emma Watson", "followers": 75, "description": "Actress"},
+    {"name": "Shah Rukh Khan", "followers": 47, "description": "Actor"},
+    {"name": "Deepika Padukone", "followers": 77, "description": "Actress"},
+    {"name": "Billie Eilish", "followers": 113, "description": "Singer"}
+]
+
+menu = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
